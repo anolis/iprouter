df3e76c363e91c6b6190631d0035c58085f1e7ae
Bill <anolisporcatus@gmail.com>
2017-07-05 19:06:17 -0500

    Tidied up script a lil more
    removed scenario specific information, added comments

--- iprouter.php
+++ iprouter.php
@@ -6,11 +6,11 @@
  * Time: 8:44 PM
  */
 
-$sheet_url = '***REMOVED***';
-$ip = $_SERVER['REMOTE_ADDR'];
-$redirect_url = 'https://www.youtube.com/watch?v=AUYqjmKstRQ';
-//echo $ip;
+$ip = $_SERVER['REMOTE_ADDR']; //requesting ip
+$sheet_url = ''; //put a google sheet url here with a function to handle the IP argument
+$redirect_url = ''; //put a redirect link here to send them to
+
+header("Location: $redirect_url" ); //send them on their way
 
 $req = curl_init($sheet_url ."?ip=$ip");
-curl_exec($req);
-header("Location: $redirect_url" );
+curl_exec($req); //run the request to the sheet
